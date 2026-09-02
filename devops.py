a6ac8b797f65810fea67b09a4c6e1a73c01a4447
Navaneetha K <navaneethakandra958@gmail.com>
2026-09-02 23:02:03 +0530

    devops

--- devops.py
+++ devops.py
@@ -1,1 +1,12 @@
-print("DevOps is awesome!")
+
+print("DevOps Tools")
+
+print("1. Git")
+print("2. GitHub")
+print("3. Jenkins")
+print("4. Ansible")
+print("5. Docker")
+print("6. Kubernetes")
+print("7. Terraform")
+print("8. Prometheus")
+print("9. Grafana")
